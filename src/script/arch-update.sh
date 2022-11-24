ddb68fb41905d2496c042f1f863569b35f067097
Antiz (Robin C.) <robincandau@protonmail.com>
2022-11-24 19:35:41 +0100

    Bump to v1.4.0 + global restructuring + added support for doas

--- src/script/arch-update.sh
+++ src/script/arch-update.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 #Current version
-version="1.3.2"
+version="1.4.0"
 
-#Check for optionnal dependencies ("yay" or "paru" for AUR support and "notify-send" (libnotify) for desktop notifications support)
-YAY=$(command -v yay)
-PARU=$(command -v paru)
-NOTIF=$(command -v notify-send)
+#Check which privilege elevation package is installed (sudo or doas)
+if command -v sudo; then
+	su_cmd="sudo"
+elif command -v doas; then
+	su_cmd="doas"
+else
+	echo -e >&2 "A privilege elevation method is required\nPlease, install sudo or doas\n" && read -n 1 -r -s -p $'Press \"enter\" to quit\n'
+	exit 1
+fi
+
+#Check if an AUR helper is installed (yay or paru) for the optional AUR package updates support
+if command -v yay; then
+	aur_helper="yay"
+elif command -v paru; then
+	aur_helper="paru"
+fi
+
+#Check if notify-send is installed for the optional desktop notification support
+notif=$(command -v notify-send)
 
 #Replace the $1 var by "option" just to make the script more readable/less complex
 option="${1}"
@@ -19,63 +34,50 @@ case "${option}" in
 		cp -f /usr/share/icons/arch-update/arch-update_checking.svg /usr/share/icons/arch-update/arch-update.svg
 		
 		#Get the available updates list for Pacman
-		PACKAGES=$(checkupdates | awk '{print $1}')
+		packages=$(checkupdates | awk '{print $1}')
 
-		#Get the available updates list for AUR (if "yay" or "paru" is installed)
-		if [ -n "${YAY}" ]; then
-			AURPACKAGES=$(yay -Qua | awk '{print $1}')
-		elif [ -n "${PARU}" ]; then
-			AURPACKAGES=$(paru -Qua | awk '{print $1}')
-		else
-			AURPACKAGES=""
+		#Get the available updates list for the AUR (if "yay" or "paru" is installed)
+		if [ -n "${aur_helper}" ]; then
+			aur_packages=$("${aur_helper}" -Qua | awk '{print $1}')
 		fi
 
 		#If there are updates available for pacman, print them
-		if [ -n "${PACKAGES}" ]; then
-			echo -e "Packages :\n" && echo -e "${PACKAGES}\n"
+		if [ -n "${packages}" ]; then
+			echo -e "Packages:\n" && echo -e "${packages}\n"
 		fi
 
-		#If there are updates available for AUR, print them
-		if [ -n "${AURPACKAGES}" ]; then
-			echo -e "AUR Packages :\n" && echo -e "${AURPACKAGES}\n"
+		#If there are updates available for the AUR, print them
+		if [ -n "${aur_packages}" ]; then
+			echo -e "AUR Packages:\n" && echo -e "${aur_packages}\n"
 		fi
 
-		#If there is no update available for Pacman nor AUR, change the desktop icon to "up-to-date" and quit
-		if [ -z "${PACKAGES}" ] && [ -z "${AURPACKAGES}" ]; then
+		#If there is no update available for Pacman nor the AUR, change the desktop icon to "up-to-date" and quit
+		if [ -z "${packages}" ] && [ -z "${aur_packages}" ]; then
 			cp -f /usr/share/icons/arch-update/arch-update_up-to-date.svg /usr/share/icons/arch-update/arch-update.svg
 			echo -e "No update available\n" && read -n 1 -r -s -p $'Press \"enter\" to quit\n'
 			exit 0
 		#If there are updates available, change the desktop icon to "updates-available" and ask the confirmation to apply them to the user
 		else
 			cp -f /usr/share/icons/arch-update/arch-update_updates-available.svg /usr/share/icons/arch-update/arch-update.svg
-			read -rp $'Proceed with installation ? [Y/n] ' answer
+			read -rp $'Proceed with installation? [Y/n] ' answer
 
 			case "${answer}" in
-				#If the user gives the confirmation, change the desktop icon to "installing" and apply updates...
+				#If the user gives the confirmation to proceed, change the desktop icon to "installing" and apply updates
 				[Yy]|"")
 					cp -f /usr/share/icons/arch-update/arch-update_installing.svg /usr/share/icons/arch-update/arch-update.svg
 
-					#...for both pacman and AUR (if there are)
-					if [ -n "${PACKAGES}" ] && [ -n "${AURPACKAGES}" ]; then
-						if [ -n "${YAY}" ]; then
-							sudo pacman -Syu && yay -Syu
-						else
-							sudo pacman -Syu && paru -Syu
-						fi
-					#... for pacman only (if there are)
-					elif [ -n "${PACKAGES}" ]; then
-						sudo pacman -Syu
-					#... for AUR only (if there are)
-					else
-						if [ -n "${YAY}" ]; then
-							yay -Syu
-						else
-							paru -Syu
-						fi
+					#Update for pacman (if there are)
+					if [ -n "${packages}" ]; then
+						"${su_cmd}" pacman -Syu
+					fi
+					
+					#Update for the AUR (if there are)
+					if [ -n "${aur_packages}" ]; then
+						"${aur_helper}" -Syu
 					fi
 				;;
 
-				#If the user didn't give the confirmation, quit
+				#If the user doesn't give the confirmation to proceed, exit
 				*)
 					exit 1
 				;;
@@ -101,23 +103,21 @@ case "${option}" in
 		cp -f /usr/share/icons/arch-update/arch-update_checking.svg /usr/share/icons/arch-update/arch-update.svg
 
 		#Get the number of available
-		if [ -n "${YAY}" ]; then
-			UPDATE_NUMBER=$( (checkupdates ; yay -Qua) | wc -l)
-		elif [ -n "${PARU}" ]; then
-			UPDATE_NUMBER=$( (checkupdates ; paru -Qua) | wc -l)
+		if [ -n "${aur_helper}" ]; then
+			update_number=$( (checkupdates ; "${aur_helper}" -Qua) | wc -l)
 		else
-			UPDATE_NUMBER=$(checkupdates | wc -l)
+			update_number=$(checkupdates | wc -l)
 		fi
 
 		#If there are updates available, change the desktop icon to "updates-available" and quit
-		if [ "${UPDATE_NUMBER}" -gt 0 ]; then
+		if [ "${update_number}" -gt 0 ]; then
 			cp -f /usr/share/icons/arch-update/arch-update_updates-available.svg /usr/share/icons/arch-update/arch-update.svg
 			#If notify-send (libnotify) is installed, also send a desktop notification before quitting
-			if [ -n "${NOTIF}" ]; then
-				if [ "${UPDATE_NUMBER}" -eq 1 ]; then
-					notify-send -i /usr/share/icons/arch-update/arch-update_updates-available.svg "Arch Update" "${UPDATE_NUMBER} update available"
+			if [ -n "${notif}" ]; then
+				if [ "${update_number}" -eq 1 ]; then
+					notify-send -i /usr/share/icons/arch-update/arch-update_updates-available.svg "Arch Update" "${update_number} update available"
 				else
-					notify-send -i /usr/share/icons/arch-update/arch-update_updates-available.svg "Arch Update" "${UPDATE_NUMBER} updates available"
+					notify-send -i /usr/share/icons/arch-update/arch-update_updates-available.svg "Arch Update" "${update_number} updates available"
 				fi
 			fi
 			exit 0
@@ -133,8 +133,8 @@ case "${option}" in
 		exit 0
 	;;
 	#If the -h (or --help) option is passed to the script, print the documentation (man page)
-	#This can be triggered directly by the user, by typing the following command in a terminal : arch-update --help
-	#The documentation is also readable here https://github.com/Antiz96/Arch-Update or by typing the following command in a terminal : man arch-update
+	#This can be triggered directly by the user, by typing the following command in a terminal: arch-update --help
+	#The documentation is also readable here https://github.com/Antiz96/Arch-Update or by typing the following command in a terminal: man arch-update
 	-h|--help)
 		#Print the documentation (man page) and quit
 		man arch-update | col
@@ -143,7 +143,7 @@ case "${option}" in
 
 	#If any other option(s) are passed to the script, print an error and quit
 	*)
-		echo -e >&2 "arch-update : invalid option -- '${option}'\nTry 'arch-update --help' for more information."
+		echo -e >&2 "arch-update: invalid option -- '${option}'\nTry 'arch-update --help' for more information."
 		exit 1
 	;;
 esac
