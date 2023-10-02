5bae268c7d9f01bc274f2cd1099acd8eafba5470
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2023-10-02 10:37:00 +0200

    Remove custom config to accept 2 trailing spaces for mdl (#51)
    https://github.com/markdownlint/markdownlint/pull/452

--- .github/workflows/mdl_style.rb
+++ .github/workflows/mdl_style.rb
@@ -3,6 +3,3 @@ all
 
 # Exclude the rules that refers to the line length
 exclude_tag :line_length
-
-# Set accepted trailing spaces to a maximum of 2 (used to break a line) for the MD009 rule
-rule 'MD009', :br_spaces => 2
