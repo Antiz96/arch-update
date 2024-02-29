d216e7116b6fa3a18dc863fda6630bd537e53fca
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2024-02-29 20:02:45 +0000

    Add shell completions for bash, zsh and fish (#118)
    * Add bash completion
    
    * Add zsh completion
    
    * Add fish completion
    
    * Add shell completions to the Makefile

---
+++ res/completions/arch-update.zsh
@@ -0,0 +1,11 @@
+#compdef arch-update
+
+local -a opts
+opts=(
+    {-c,--check}'[Check for available updates]'
+    {-n,--news}'[Display latest Arch news]'
+    {-h,--help}'[Display the help message]'
+    {-V,--version}'[Display version information]'
+)
+
+_arguments $opts
