a5ace4ca5434cb044bf214ca928a341cd49950ca
Sparkway <84066435+Sparkway@users.noreply.github.com>
2024-03-17 11:22:21 +0100

    Add the `-l/--list` option to shell completions (#127)
    * Add -l/--list to bash completion
    
    * Add -l/--list to fish completion
    
    * Add -l/--list to zsh completion

--- res/completions/arch-update.fish
+++ res/completions/arch-update.fish
@@ -1,6 +1,7 @@
 complete -c arch-update -f
 
 complete -c arch-update -s c -l check -d 'Check for available updates'
+complete -c arch-update -s l -l list -d 'Display the list of pending updates'
 complete -c arch-update -s n -l news -d 'Display latest Arch news'
 complete -c arch-update -s h -l help -d 'Display the help message'
 complete -c arch-update -s V -l version -d 'Display version information'
