fcdacad8650576f4ad48f416f765dbceba424a9a
Robin Candau <robincandau@protonmail.com>
2024-03-21 20:50:35 +0100

    Change wording in shell completions

--- res/completions/arch-update.fish
+++ res/completions/arch-update.fish
@@ -2,7 +2,7 @@ complete -c arch-update -f
 
 complete -c arch-update -s c -l check -d 'Check for available updates'
 complete -c arch-update -s l -l list -d 'Display the list of pending updates'
-complete -c arch-update -s d -l devel -d 'Update AUR development packages'
+complete -c arch-update -s d -l devel -d 'Also check AUR development packages updates'
 complete -c arch-update -s n -l news -d 'Display latest Arch news'
 complete -c arch-update -s h -l help -d 'Display the help message'
 complete -c arch-update -s V -l version -d 'Display version information'
