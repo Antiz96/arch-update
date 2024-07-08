27b563df3fe46b01333ba01748fcd91e7a709abf
Robin Candau <robincandau@protonmail.com>
2024-07-08 19:45:29 +0200

    Fix: Fix syntax in translation files

--- src/script/arch-update.sh
+++ src/script/arch-update.sh
@@ -844,7 +844,7 @@ case "${option}" in
 			exit 13
 		else
 			if ! "${EDITOR:-nano}" "${config_file}" 2> /dev/null; then
-				error_msg "$(eval_gettext "Unable to determine the editor to use\nThe \$EDITOR environment variable is not set and \"nano\" (fallback option) is not installed")"
+				error_msg "$(eval_gettext "Unable to determine the editor to use\nThe \"EDITOR\" environment variable is not set and \"nano\" (fallback option) is not installed")"
 				exit 13
 			fi
 		fi
