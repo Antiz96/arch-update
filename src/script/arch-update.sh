a16a160f3fe53567cc83f5d6a697c0a3eebcb69d
Robin Candau <robincandau@protonmail.com>
2024-07-09 13:03:33 +0200

    fix: Optimize the "News" function and avoid unexpected behaviors (#213)
    This commit brings multiple improvements to the "News" function of the main script in order to optimize it and avoid unexpected behaviors:
    
    - Print a message when Arch-Update is looking for recent Arch News (to avoid people thinking the script is just stuck in case it takes some time due to network instability/slowness)
    - Print a message if no recent Arch News has been found since the last run
    - Optimize the curl requests by merging the two first ones into a single one
    - Add a default 30 sec timeout (`--max-time 30`) to curl requests so they don't end taking multiple minutes on slow/faulty network connection). If the timeout is reached, Arch-Update prints a warning explaining that it wasn't able to retrieve Arch News in a reasonable time, prints the URL of the Arch website or the URL of the selected news (so users can still look that up separately) and move on to the packages updates.
    
    Fixes https://github.com/Antiz96/arch-update/issues/206

--- src/script/arch-update.sh
+++ src/script/arch-update.sh
@@ -341,6 +341,7 @@ list_packages() {
 			case "${answer}" in
 				"$(eval_gettext "Y")"|"$(eval_gettext "y")"|"")
 					proceed_with_update="y"
+					echo
 				;;
 				*)
 					error_msg "$(eval_gettext "The update has been aborted\n")" && quit_msg
@@ -353,76 +354,92 @@ list_packages() {
 
 # Definition of the list_news function: Display the latest Arch news and offers to read them
 list_news() {
-	if [ -z "${show_news}" ]; then
-		curl -Ls https://www.archlinux.org/news | htmlq -a title a | grep ^"View:" | sed "s/View:\ //g" | head -1 > "${statedir}/current_news_check"
+	info_msg "$(eval_gettext "Looking for recent Arch News...")"
+	news=$(curl -m 30 -Ls https://www.archlinux.org/news || echo "timeout")
 
-		if ! diff "${statedir}/current_news_check" "${statedir}/last_news_check" &> /dev/null; then
-			show_news="y"
-		fi
-
-		if [ -f "${statedir}/current_news_check" ]; then
-			mv -f "${statedir}/current_news_check" "${statedir}/last_news_check"
-		fi
-	fi
-
-	if [ -n "${show_news}" ]; then
-		news=$(curl -Ls https://www.archlinux.org/news)
-		news_titles=$(echo "${news}" | htmlq -a title a | grep ^"View:" | sed "s/View:\ //g" | head -"${news_num}")
-		mapfile -t news_dates < <(echo "${news}" | htmlq td | grep -v "class" | grep "[0-9]" | sed "s/<[^>]*>//g" | head -"${news_num}" | xargs -I{} date -d "{}" "+%s")
-
+	if [ "${news}" == "timeout" ]; then
 		echo
-		main_msg "$(eval_gettext "Arch News:")"
-
-		i=1
-		while IFS= read -r line; do
-			if [ -z "${news_option}" ] && [ "${news_dates["${i}-1"]}" -ge "$(date -d "$(cat "${statedir}/last_update_run" 2> /dev/null)" +%s)" ]; then
-				new_tag="$(eval_gettext "[NEW]")"
-				echo -e "${i} - ${line} ${green}${new_tag}${color_off}"
+		warning_msg "$(eval_gettext "Unable to retrieve recent Arch News within a reasonable time (possibly because of a slow or faulty network connection)\nPlease, look for any recent news at https://archlinux.org before updating your system")"
+	else
+		if [ -z "${show_news}" ]; then
+			echo "${news}" | htmlq -a title a | grep ^"View:" | sed "s/View:\ //g" | head -1 > "${statedir}/current_news_check"
+	
+			if ! diff "${statedir}/current_news_check" "${statedir}/last_news_check" &> /dev/null; then
+				show_news="y"
 			else
-				echo "${i} - ${line}"
+				echo
+				info_msg "$(eval_gettext "No recent Arch News found")"
 			fi
-			((i=i+1))
-		done < <(printf '%s\n' "${news_titles}")
-
-		echo
-
-		if [ -n "${news_option}" ]; then
-			ask_msg_array "$(eval_gettext "Select the news to read (e.g. 1 3 5), select 0 to read them all or press \"enter\" to quit:")"
-		else
-			ask_msg_array "$(eval_gettext "Select the news to read (e.g. 1 3 5), select 0 to read them all or press \"enter\" to proceed with update:")"
-		fi
-
-		if [ "${answer_array[0]}" -eq 0 ] 2> /dev/null; then
-			answer_array=()
-			for ((i=1; i<=news_num; i++)); do
-				answer_array+=("${i}")
-			done
-		else
-			array_to_string=$(printf "%s\n" "${answer_array[@]}")
-			mapfile -t answer_array < <(echo "${array_to_string}" | awk '!seen[$0]++')
-		fi
-
-		for num in "${answer_array[@]}"; do
-			if [ "${num}" -le "${news_num}" ] 2> /dev/null && [ "${num}" -gt "0" ]; then
-				news_selected=$(sed -n "${num}"p <<< "${news_titles}")
-				news_path=$(echo "${news_selected}" | sed s/\ -//g | sed s/\ /-/g | sed s/[.]//g | sed s/=//g | sed s/\>//g | sed s/\<//g | sed s/\`//g | sed s/://g | sed s/+//g | sed s/[[]//g | sed s/]//g | sed s/,//g | sed s/\(//g | sed s/\)//g | sed s/[/]//g | sed s/@//g | sed s/\'//g | sed s/--/-/g | awk '{print tolower($0)}')
-				news_url="https://www.archlinux.org/news/${news_path}"
-				news_content=$(curl -Ls "${news_url}")
-				news_author=$(echo "${news_content}" | htmlq -t .article-info | cut -f3- -d " ")
-				news_date=$(echo "${news_content}" | htmlq -t .article-info | cut -f1 -d " ")
-				news_article=$(echo "${news_content}" | htmlq -t .article-content)
-				title_tag="$(eval_gettext "Title:")"
-				author_tag="$(eval_gettext "Author:")"
-				publication_date_tag="$(eval_gettext "Publication date:")"
-				url_tag="$(eval_gettext "URL:")"
-				echo -e "\n${blue}---${color_off}\n${bold}${title_tag}${color_off} ${news_selected}\n${bold}${author_tag}${color_off} ${news_author}\n${bold}${publication_date_tag}${color_off} ${news_date}\n${bold}${url_tag}${color_off} ${news_url}\n${blue}---${color_off}\n\n${news_article}"
-				printed_news="y"
+	
+			if [ -f "${statedir}/current_news_check" ]; then
+				mv -f "${statedir}/current_news_check" "${statedir}/last_news_check"
 			fi
-		done
-
-		if [ -z "${news_option}" ] && [ -n "${printed_news}" ]; then
+		fi
+	
+		if [ -n "${show_news}" ]; then
+			news_titles=$(echo "${news}" | htmlq -a title a | grep ^"View:" | sed "s/View:\ //g" | head -"${news_num}")
+			mapfile -t news_dates < <(echo "${news}" | htmlq td | grep -v "class" | grep "[0-9]" | sed "s/<[^>]*>//g" | head -"${news_num}" | xargs -I{} date -d "{}" "+%s")
+	
 			echo
-			continue_msg
+			main_msg "$(eval_gettext "Arch News:")"
+	
+			i=1
+			while IFS= read -r line; do
+				if [ -z "${news_option}" ] && [ "${news_dates["${i}-1"]}" -ge "$(date -d "$(cat "${statedir}/last_update_run" 2> /dev/null)" +%s)" ]; then
+					new_tag="$(eval_gettext "[NEW]")"
+					echo -e "${i} - ${line} ${green}${new_tag}${color_off}"
+				else
+					echo "${i} - ${line}"
+				fi
+				((i=i+1))
+			done < <(printf '%s\n' "${news_titles}")
+	
+			echo
+	
+			if [ -n "${news_option}" ]; then
+				ask_msg_array "$(eval_gettext "Select the news to read (e.g. 1 3 5), select 0 to read them all or press \"enter\" to quit:")"
+			else
+				ask_msg_array "$(eval_gettext "Select the news to read (e.g. 1 3 5), select 0 to read them all or press \"enter\" to proceed with update:")"
+			fi
+	
+			if [ "${answer_array[0]}" -eq 0 ] 2> /dev/null; then
+				answer_array=()
+				for ((i=1; i<=news_num; i++)); do
+					answer_array+=("${i}")
+				done
+			else
+				array_to_string=$(printf "%s\n" "${answer_array[@]}")
+				mapfile -t answer_array < <(echo "${array_to_string}" | awk '!seen[$0]++')
+			fi
+	
+			for num in "${answer_array[@]}"; do
+				if [ "${num}" -le "${news_num}" ] 2> /dev/null && [ "${num}" -gt "0" ]; then
+					printed_news="y"
+					news_selected=$(sed -n "${num}"p <<< "${news_titles}")
+					news_path=$(echo "${news_selected}" | sed s/\ -//g | sed s/\ /-/g | sed s/[.]//g | sed s/=//g | sed s/\>//g | sed s/\<//g | sed s/\`//g | sed s/://g | sed s/+//g | sed s/[[]//g | sed s/]//g | sed s/,//g | sed s/\(//g | sed s/\)//g | sed s/[/]//g | sed s/@//g | sed s/\'//g | sed s/--/-/g | awk '{print tolower($0)}')
+					news_url="https://www.archlinux.org/news/${news_path}"
+					news_content=$(curl -m 30 -Ls "${news_url}" || echo "timeout")
+
+					if [ "${news_content}" == "timeout" ]; then
+						echo
+						warning_msg "$(eval_gettext "Unable to retrieve the selected Arch News within a reasonable time (possibly because of a slow or faulty network connection)\nPlease, read the selected Arch News at \${news_url} before updating your system")"
+					else
+						news_author=$(echo "${news_content}" | htmlq -t .article-info | cut -f3- -d " ")
+						news_date=$(echo "${news_content}" | htmlq -t .article-info | cut -f1 -d " ")
+						news_article=$(echo "${news_content}" | htmlq -t .article-content)
+						title_tag="$(eval_gettext "Title:")"
+						author_tag="$(eval_gettext "Author:")"
+						publication_date_tag="$(eval_gettext "Publication date:")"
+						url_tag="$(eval_gettext "URL:")"
+						echo -e "\n${blue}---${color_off}\n${bold}${title_tag}${color_off} ${news_selected}\n${bold}${author_tag}${color_off} ${news_author}\n${bold}${publication_date_tag}${color_off} ${news_date}\n${bold}${url_tag}${color_off} ${news_url}\n${blue}---${color_off}\n\n${news_article}"
+					fi
+				fi
+			done
+	
+			if [ -z "${news_option}" ] && [ -n "${printed_news}" ]; then
+				echo
+				continue_msg
+			fi
 		fi
 	fi
 }
