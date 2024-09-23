5692902f47e3d88eccbea01743e87a1c772711dc
Robin Candau <robincandau@protonmail.com>
2024-09-23 21:41:34 +0200

    fix: Fix pgrep pattern to avoid having multiple systrays at once

--- src/lib/tray.sh
+++ src/lib/tray.sh
@@ -37,7 +37,8 @@ else
 		state_up_to_date
 	fi
 
-	if pgrep -f arch-update-tray > /dev/null; then
+	# shellcheck disable=SC2154
+	if pgrep -f "${libdir}/tray.py" > /dev/null; then
 		error_msg "$(eval_gettext "There's already a running instance of the Arch-Update systray applet")"
 		exit 3
 	fi
