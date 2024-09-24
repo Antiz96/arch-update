b67c474b04bfa77aa9091c1a9f971232e2c0818d
Robin Candau <robincandau@protonmail.com>
2024-09-24 13:37:07 +0200

    chore: Rename "state" functions & file to "(tray) icon"
    The "current_state" file and the related functions are in fact only used to determine which icon the systray applet should display (depending on the current state of the system).
    Recording the current state of the system (up to date/updates available) is not necessary for any other stages (not currently at least), so it makes sense adopting a more suited & precise name.

--- src/lib/list_packages.sh
+++ src/lib/list_packages.sh
@@ -44,14 +44,14 @@ if [ -n "${flatpak_packages}" ]; then
 fi
 
 if [ -z "${packages}" ] && [ -z "${aur_packages}" ] && [ -z "${flatpak_packages}" ]; then
-	state_up_to_date
+	icon_up-to-date
 	info_msg "$(eval_gettext "No update available\n")"
 
 	if [ -n "${list_option}" ]; then
 		exit 7
 	fi
 else
-	state_updates_available
+	icon_updates-available
 	if [ -z "${list_option}" ]; then
 		ask_msg "$(eval_gettext "Proceed with update? [Y/n]")"
 
