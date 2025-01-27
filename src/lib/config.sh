ac12f95304e9162755841eb8c777a143835f1340
Robin Candau <robincandau@protonmail.com>
2025-01-27 06:39:01 +0100

    feat: Add the `NoFlatpak` option to `arch-update.conf`
    Add the `NoFlatpak` option in the `arch-update.conf` configuration file to ignore Flatpak packages support (even if `flatpak` is installed).

--- src/lib/config.sh
+++ src/lib/config.sh
@@ -18,6 +18,10 @@ if [ -f "${config_file}" ]; then
 	# shellcheck disable=SC2034
 	no_version=$(grep -Eq '^[[:space:]]*NoVersion[[:space:]]*$' "${config_file}" 2> /dev/null && echo "true")
 
+	# Check the "NoFlatpak" option in arch-update.conf
+	# shellcheck disable=SC2034
+	no_flatpak=$(grep -Eq '^[[:space:]]*NoFlatpak[[:space:]]*$' "${config_file}" 2> /dev/null && echo "true")
+
 	# Check the "NoNotification" option in arch-update.conf
 	# shellcheck disable=SC2034
 	no_notification=$(grep -Eq '^[[:space:]]*NoNotification[[:space:]]*$' "${config_file}" 2> /dev/null && echo "true")
