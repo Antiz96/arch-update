9f39405dce8bde32c87bce7b6b42b437f31c4a61
Robin Candau <robincandau@protonmail.com>
2025-07-16 21:21:30 +0200

    feat(notification): Add click actions to run Arch-Update and dismiss notifications
    Add click actions in desktop notifications to allow running Arch-Update and dismiss notifications directly from it.
    Also associate properly desktop notifications to the Arch-Update application.

--- src/lib/check.sh
+++ src/lib/check.sh
@@ -44,27 +44,54 @@ if [ -n "${update_available}" ]; then
 	icon_updates-available
 
 	if [ -n "${notification_support}" ]; then
+		(
 		if ! diff "${statedir}/current_updates_check" "${statedir}/last_updates_check" &> /dev/null; then
 			update_number=$(wc -l "${statedir}/current_updates_check" | awk '{print $1}')
+
 			# shellcheck disable=SC2154
-			last_notif_id=$(cat "${tmpdir}/last_notif_id" 2> /dev/null)
+			last_notif_id=$(sed -n '1p' "${tmpdir}/notif_param" 2> /dev/null)
+
 			if [ "${update_number}" -eq 1 ]; then
 				if [ -z "${last_notif_id}" ]; then
 					# shellcheck disable=SC2154
-					notify-send -p -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} update available")" > "${tmpdir}/last_notif_id"
+					notify-send -p -a "${_name}" -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} update available")" -A "run=$(eval_gettext "Run Arch-Update")" -A "close=$(eval_gettext "Close")" > "${tmpdir}/notif_param"
 				else
 					# shellcheck disable=SC2154
-					notify-send -p -r "${last_notif_id}" -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} update available")" > "${tmpdir}/last_notif_id"
+					notify-send -p -r "${last_notif_id}" -a "${_name}" -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} update available")" -A "run=$(eval_gettext "Run Arch-Update")" -A "close=$(eval_gettext "Close")" > "${tmpdir}/notif_param"
 				fi
-
 			else
 				if [ -z "${last_notif_id}" ]; then
-					notify-send -p -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} updates available")" > "${tmpdir}/last_notif_id"
+					notify-send -p -a "${_name}" -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} updates available")" -A "run=$(eval_gettext "Run Arch-Update")" -A "close=$(eval_gettext "Close")" > "${tmpdir}/notif_param"
 				else
-					notify-send -p -r "${last_notif_id}" -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} updates available")" > "${tmpdir}/last_notif_id"
+					notify-send -p -r "${last_notif_id}" -a "${_name}" -i "${name}_updates-available-${tray_icon_style}" "${_name}" "$(eval_gettext "\${update_number} updates available")" -A "run=$(eval_gettext "Run Arch-Update")" -A "close=$(eval_gettext "Close")" > "${tmpdir}/notif_param"
+				fi
+			fi
+			
+			# shellcheck disable=SC2154
+			if [ -f "${XDG_DATA_HOME}/applications/${name}.desktop" ]; then
+				desktop_file="${XDG_DATA_HOME}/applications/${name}.desktop"
+			elif [ -f "${HOME}/.local/share/applications/${name}.desktop" ]; then
+				desktop_file="${HOME}/.local/share/applications/${name}.desktop"
+			elif [ -f "${XDG_DATA_DIRS}/applications/${name}.desktop" ]; then
+				desktop_file="${XDG_DATA_DIRS}/applications/${name}.desktop"
+			elif [ -f "/usr/local/share/applications/${name}.desktop" ]; then
+				desktop_file="/usr/local/share/applications/${name}.desktop"
+			elif [ -f "/usr/share/applications/${name}.desktop" ]; then
+				desktop_file="/usr/share/applications/${name}.desktop"
+			else
+				error_msg "$(eval_gettext "Arch-Update desktop file not found")"
+				exit 18
+			fi
+
+			if [ "$(sed -n '2p' "${tmpdir}/notif_param")" == "run" ]; then
+				exec 9>"${tmpdir}/notif_action.lock"
+
+				if flock -n 9; then
+					gio launch "${desktop_file}" || exit 18
 				fi
 			fi
 		fi
+		) & disown
 	fi
 else
 	icon_up-to-date
