5fd5e008cd2558e3598648dc86b43e65dd983af2
Robin Candau <robincandau@protonmail.com>
2025-11-18 00:37:35 +0100

    chore(release): Improve the release process
    Add a dedicated script to cut new releases

--- RELEASE.md
+++ RELEASE.md
@@ -1,53 +1,24 @@
-# Creating a release
+# Creating a new release
 
-- Make sure that `git-cliff` is installed and `arch-update` is up to date on your system.
+## Required dependencies
 
-- Export a `TAG` variable containing the new tag for the release:
+- `git-cliff`
+- `github-cli`
 
-```bash
-export TAG="X.Y.Z"
-```
+## Disable the main branch protection rule temporarily
 
-- Bump version where necessary:
+<https://github.com/Antiz96/arch-update/settings/branches>
 
-```bash
-sed -i "s/$(arch-update -V | cut -f2 -d " ")/${TAG}/g" doc/man/arch-update.* doc/man/fr/arch-update.* po/* src/arch-update.sh
-```
+## Run the script
 
-- Update changelog:
+From the root of the repo:
 
 ```bash
-git-cliff -up CHANGELOG.md
-sed -i "s|\[unreleased\]|\[v${TAG}\](https://github.com/Antiz96/arch-update/releases/tag/v${TAG})\ -\ $(date '+%Y-%m-%d')|g" CHANGELOG.md
+./scripts/mkrelease.sh X.Y.Z
 ```
 
-- Commit, sign and push changes directly to the **main branch** *(we want the tagged commit to be signed with the [OpenPGP Key](https://keyserver.ubuntu.com/pks/lookup?search=D33FAA16B937F3B2&fingerprint=on&op=index) listed in [MAINTAINERS.md](https://github.com/Antiz96/arch-update/blob/main/MAINTAINERS.md) and not with the GitHub signature key automatically used when merging a pull request; this implies **temporarily** allowing administrators to bypass branch protections rules)*:
+Where `X.Y.Z` is the version / tag number to create (example: 3.16.2)
 
-```bash
-git add .
-git commit -SD33FAA16B937F3B2 -m "chore(release): v${TAG}"
-git push
-```
+## Re-enable the main branch protection rule
 
-- Create, sign and push the new tag:
-
-```bash
-git tag v${TAG} -u D33FAA16B937F3B2 -m "v${TAG}"
-git push origin v${TAG}
-```
-
-- Create a release on GitHub, download and sign the auto-generated source tarball:
-
-```bash
-cd ~/Downloads
-gpg --local-user D33FAA16B937F3B2 --armor --detach-sign arch-update-${TAG}.tar.gz
-sha256sum arch-update-${TAG}.tar.gz > arch-update-${TAG}.tar.gz.sha256
-gpg --local-user D33FAA16B937F3B2 --armor --detach-sign arch-update-${TAG}.tar.gz.sha256
-rm -f arch-update-${TAG}.tar.gz
-```
-
-- Upload the 3 produced files as assets in the release, re-enable the branch protection rules on the main branch for administrators and unset the `TAG` variable:
-
-```bash
-unset TAG
-```
+<https://github.com/Antiz96/arch-update/settings/branches>
---
+++ scripts/mkrelease.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -euo pipefail
+
+# Check that we are on the main branch
+if [ "$(git branch --show-current)" != "main" ]; then
+	echo -e >&2 "ERROR: Current branch is not 'main'"
+	exit 1
+fi
+
+# Check that `github-cli` is correctly authenticated
+if ! gh auth status > /dev/null; then
+	echo -e >&2 "ERROR: github-cli is not authenticated"
+	exit 2
+fi
+
+# Pull repo and fetch tags
+git pull
+git fetch --tags
+
+# Assign and check tag variables
+release_tag="${1}"
+latest_tag=$(git describe --tags --abbrev=0)
+
+if [ -z "${release_tag}" ]; then
+	echo -e >&2 "ERROR: Release tag is empty\nUsage: ./scripts/mkrelease.sh X.Y.Z # where 'X.Y.Z' is the tag to create"
+	exit 3
+fi
+
+echo -e "Release tag = v${release_tag}\nLatest tag = ${latest_tag}\n"
+read -rp "Confirm? [y/N] " answer
+
+case "${answer}" in
+	y|Y)
+		echo -e "\nProceeding in 5 sec"
+		sleep 5
+	;;
+	*)
+		echo -e >&2 "\nAborting"
+		exit 4
+	;;
+esac
+
+
+# Bump version where necessary
+sed -i "s/${latest_tag#v}/${release_tag}/g" doc/man/arch-update.* doc/man/fr/arch-update.* po/* src/arch-update.sh
+
+# Update changelog
+git-cliff -up CHANGELOG.md
+sed -i "s|\[unreleased\]|\[v${release_tag}\](https://github.com/Antiz96/arch-update/releases/tag/v${release_tag})\ -\ $(date '+%Y-%m-%d')|g" CHANGELOG.md
+
+# Review changes
+git diff
+echo
+read -rp "Confirm? [y/N] " answer
+
+case "${answer}" in
+	y|Y)
+		echo -e "\nProceeding in 5 sec"
+		sleep 5
+	;;
+	*)
+		echo -e >&2 "\nAborting"
+		exit 5
+	;;
+esac
+
+# Create and push a signed commit
+git add .
+git commit -SD33FAA16B937F3B2 -m "chore(release): v${release_tag}"
+git push
+
+# Create and push a signed tag
+git tag "v${release_tag}" -u D33FAA16B937F3B2 -m "v${release_tag}"
+git push origin "v${release_tag}"
+
+# Create release
+gh release create "v${release_tag}" --verify-tag -F -
+
+# Download and sign auto-generated source tarball and checksum
+gh release download "v${release_tag}" --archive tar.gz --clobber
+gpg --local-user D33FAA16B937F3B2 --armor --detach-sign "arch-update-${release_tag}.tar.gz"
+sha256sum "arch-update-${release_tag}.tar.gz" > "arch-update-${release_tag}.tar.gz.sha256"
+gpg --local-user D33FAA16B937F3B2 --armor --detach-sign "arch-update-${release_tag}.tar.gz.sha256"
+
+# Upload source tarball and checksum signatures
+gh release upload "v${release_tag}" "arch-update-${release_tag}.tar.gz.asc" "arch-update-${release_tag}.tar.gz.sha256" "arch-update-${release_tag}.tar.gz.sha256.asc"
+rm -f "arch-update-${release_tag}.tar.gz"*
