ddbeafeb4fb3ff901ac7ecac8a491f2db628e5de
Robin Candau <robincandau@protonmail.com>
2026-03-25 13:02:10 +0100

    feat: Add a default timeout for update check requests
    Flatpak has been frequently suffering from servers latency / downtime lately, causing Flatpak updates checks performed by Arch-Update to be extremely slow (as Flatpak remote servers were not responding).
    
    As such, we are adding a default timeout of 30 seconds (by default) for updates checks, in order to avoid having Arch-Update seemingly hanging for a long time when such eventual issue occurs. The timeout is not only applied to Flatpak updates checks  but also for repo and AUR packages updates checks (as those can theoretically suffer from the same issues in specific cases, e.g. when Arch / AUR suffered from DDOS).
    
    The timeout value can be customized with the new "UpdateCheckTimeout" option in the `arch-update.conf` configuration file (default to 30 sec).
    
    Also, change the related "error" string state by a less generic value ("echo_during_request") to avoid eventual false positives.

--- src/lib/list_packages.sh
+++ src/lib/list_packages.sh
@@ -8,61 +8,70 @@ info_msg "$(eval_gettext "Looking for updates...\n")"
 
 # shellcheck disable=SC2154
 checkupdates_db_tmpdir=$(mktemp -d "${checkupdates_db_tmpdir_prefix}XXXXX")
+packages=$(CHECKUPDATES_DB="${checkupdates_db_tmpdir}" timeout "${update_check_timeout}" checkupdates "${contrib_color_opt[@]}" || echo "error_during_request")
 
-if [ -z "${no_version}" ]; then
-	# shellcheck disable=SC2154
-	packages=$(CHECKUPDATES_DB="${checkupdates_db_tmpdir}" timeout "${update_check_timeout}" checkupdates "${contrib_color_opt[@]}" || echo "error")
-else
-	# shellcheck disable=SC2154
-	packages=$(CHECKUPDATES_DB="${checkupdates_db_tmpdir}" timeout "${update_check_timeout}" checkupdates "${contrib_color_opt[@]}" | awk '{print $1}' || echo "error")
+if [ "${packages}" == "error_during_request" ]; then
+	package_type="Packages"
+	warning_msg "$(eval_gettext "Unable to retrieve ${package_type} updates (error response or request timeout\n)")"
+	unset packages
+elif [ -n "${no_version}" ]; then
+	packages=$(echo "${packages}" | awk '{print $1}')
 fi
 
 if [ -n "${aur_helper}" ]; then
-	if [ -z "${no_version}" ]; then
-		# shellcheck disable=SC2154
-		aur_packages=$(timeout "${update_check_timeout}" "${aur_helper}" --color "${pacman_color_opt}" "${devel_flag[@]}" -Qua 2> /dev/null | sed 's/^ *//' | sed 's/ \+/ /g' | grep -vw "\[ignored\]$" || echo "error")
-	else
-		# shellcheck disable=SC2154
-		aur_packages=$(timeout "${update_check_timeout}" "${aur_helper}" --color "${pacman_color_opt}" "${devel_flag[@]}" -Qua 2> /dev/null | sed 's/^ *//' | sed 's/ \+/ /g' | grep -vw "\[ignored\]$" | awk '{print $1}' || echo "error")
+	unformatted_aur_packages=$(timeout "${update_check_timeout}" "${aur_helper}" --color "${pacman_color_opt}" "${devel_flag[@]}" -Qua 2> /dev/null || echo "error_during_request")
+	aur_packages=$(echo "${unformatted_aur_packages}" | sed 's/^ *//' | sed 's/ \+/ /g' | grep -vw "\[ignored\]$")
+
+	if [ "${aur_packages}" == "error_during_request" ]; then
+		package_type="AUR Packages"
+		warning_msg "$(eval_gettext "Unable to retrieve ${package_type} updates (error response or request timeout\n)")"
+		unset aur_packages
+	elif [ -n "${no_version}" ]; then
+		aur_packages=$(echo "${aur_packages}" | awk '{print $1}')
 	fi
 fi
 
 if [ -n "${flatpak_support}" ]; then
-	flatpak_metadata_state=$(timeout "${update_check_timeout}" flatpak update --appstream > /dev/null || echo "error")
+	flatpak_metadata_update=$(timeout "${update_check_timeout}" flatpak update --appstream > /dev/null || echo "error_during_request")
 
-	mapfile -t flatpak_mask < <(flatpak mask | tr -d ' ')
-
-	if [ "${#flatpak_mask[@]}" -gt 0 ]; then
-		mapfile -t flatpak_packages < <(flatpak remote-ls --updates --cached --columns=application,version | tr -s '\t' ' ')
-
-		declare -A app_names
-		while read -r flatpak_id flatpak_name; do
-			app_names["${flatpak_id}"]="${flatpak_name}"
-		done < <(flatpak list --columns=application,name)
-
-	        mapfile -t flatpak_packages < <(
-			for packages in "${flatpak_packages[@]}"; do
-				read -r app_id app_version <<< "${packages}"
-
-				for pattern in "${flatpak_mask[@]}"; do
-					# shellcheck disable=SC2053
-					[[ "${app_id}" == ${pattern} ]] && continue 2
-				done
-
-				app_name="${app_names[${app_id}]:-${app_id}}"
-
-				if [ -z "${no_version}" ]; then
-					echo "${app_name} ${app_version}"
-				else
-					echo "${app_name}"
-				fi
-			done
-		)
+	if [ "${flatpak_metadata_update}" == "error_during_request" ]; then
+		package_type="Flatpak Packages"
+		warning_msg "$(eval_gettext "Unable to retrieve ${package_type} updates (error response or request timeout\n)")"
 	else
-		if [ -z "${no_version}" ]; then
-			mapfile -t flatpak_packages < <(flatpak remote-ls --updates --cached --columns=name,version | tr -s '\t' ' ')
+		mapfile -t flatpak_mask < <(flatpak mask | tr -d ' ')
+
+		if [ "${#flatpak_mask[@]}" -gt 0 ]; then
+			mapfile -t flatpak_packages < <(flatpak remote-ls --updates --cached --columns=application,version | tr -s '\t' ' ')
+
+			declare -A app_names
+			while read -r flatpak_id flatpak_name; do
+				app_names["${flatpak_id}"]="${flatpak_name}"
+			done < <(flatpak list --columns=application,name)
+
+			mapfile -t flatpak_packages < <(
+				for packages in "${flatpak_packages[@]}"; do
+					read -r app_id app_version <<< "${packages}"
+
+					for pattern in "${flatpak_mask[@]}"; do
+						# shellcheck disable=SC2053
+						[[ "${app_id}" == ${pattern} ]] && continue 2
+					done
+
+					app_name="${app_names[${app_id}]:-${app_id}}"
+
+					if [ -z "${no_version}" ]; then
+						echo "${app_name} ${app_version}"
+					else
+						echo "${app_name}"
+					fi
+				done
+			)
 		else
-			mapfile -t flatpak_packages < <(flatpak remote-ls --updates --cached --columns=name)
+			if [ -z "${no_version}" ]; then
+				mapfile -t flatpak_packages < <(flatpak remote-ls --updates --cached --columns=name,version | tr -s '\t' ' ')
+			else
+				mapfile -t flatpak_packages < <(flatpak remote-ls --updates --cached --columns=name)
+			fi
 		fi
 	fi
 fi
@@ -73,30 +82,21 @@ true > "${statedir}/last_updates_check_packages"
 true > "${statedir}/last_updates_check_aur"
 true > "${statedir}/last_updates_check_flatpak"
 
-if [ "${packages}" == "error" ]; then
-	warning_msg "$(eval_gettext "Unable to retrieve Packages updates (error response or request timeout\n)")"
-	unset packages
-elif [ -n "${packages}" ]; then
+if [ -n "${packages}" ]; then
 	main_msg "$(eval_gettext "Packages:")"
 	echo -e "${packages}\n"
 	echo "${packages}" >> "${statedir}/last_updates_check"
 	echo "${packages}" > "${statedir}/last_updates_check_packages"
 fi
 
-if [ "${aur_packages}" == "error" ]; then
-	warning_msg "$(eval_gettext "Unable to retrieve AUR Packages updates (error response or request timeout\n)")"
-	unset aur_packages
-elif [ -n "${aur_packages}" ]; then
+if [ -n "${aur_packages}" ]; then
 	main_msg "$(eval_gettext "AUR Packages:")"
 	echo -e "${aur_packages}\n"
 	echo "${aur_packages}" >> "${statedir}/last_updates_check"
 	echo "${aur_packages}" > "${statedir}/last_updates_check_aur"
 fi
 
-if [ "${flatpak_metadata_state}" == "error" ]; then
-	warning_msg "$(eval_gettext "Unable to retrieve Flatpak Packages updates (error response or request timeout\n)")"
-	unset flatpak_packages
-elif [ "${#flatpak_packages[@]}" -gt 0 ]; then
+if [ "${#flatpak_packages[@]}" -gt 0 ]; then
 	main_msg "$(eval_gettext "Flatpak Packages:")"
 	printf "%s\n" "${flatpak_packages[@]}" ""
 	printf "%s\n" "${flatpak_packages[@]}" >> "${statedir}/last_updates_check"
