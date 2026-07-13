6300b8793884f571add131fd9407178bd7a604c1
Robin Candau <robincandau@protonmail.com>
2026-07-13 12:47:51 +0200

    Simplify 'true' call in list_packages.sh

--- src/lib/list_packages.sh
+++ src/lib/list_packages.sh
@@ -83,10 +83,7 @@ if [ -n "${flatpak_support}" ]; then
 fi
 
 # shellcheck disable=SC2154
-true > "${statedir}/last_updates_check"
-true > "${statedir}/last_updates_check_packages"
-true > "${statedir}/last_updates_check_aur"
-true > "${statedir}/last_updates_check_flatpak"
+true > "${statedir}"/last_updates_check{,_packages,_aur,_flatpak}
 
 # Re-color update list output with version diff highlighting
 color_update_list() {
