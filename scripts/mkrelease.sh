61e6043d81cd87f6f77b22946707ad220a38f18c
Robin Candau <robincandau@protonmail.com>
2026-07-29 18:07:42 +0200

    chore(release): Add versioning to tray's Cargo.toml

--- scripts/mkrelease.sh
+++ scripts/mkrelease.sh
@@ -43,6 +43,7 @@ esac
 # Bump version where necessary
 sed_pattern="${latest_tag//./\\.}" # escape dots
 sed -i "s/${sed_pattern#v}/${release_tag}/g" doc/man/arch-update.* po/* src/arch-update.sh
+sed -i "s/version = \"${sed_pattern#v}\"/version = \"${release_tag}\"/g" Cargo.toml
 
 # Update changelog
 git-cliff -up CHANGELOG.md
